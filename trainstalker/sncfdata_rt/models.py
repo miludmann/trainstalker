9a307ea3b2cca2c37ff461f9e4834d84c0dd6dbc
Michael Ludmann <miludmann@gmail.com>
2015-01-13 17:45:50 +0100

    Add details to model and fixe timezone issues

--- trainstalker/sncfdata_rt/models.py
+++ trainstalker/sncfdata_rt/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from datetime import datetime, timedelta
 from django.utils import timezone
 
 # Create your models here.
@@ -9,9 +8,10 @@ class Station(models.Model):
     '''Train station'''
     station_name = models.CharField(max_length=100)
     station_code = models.CharField(max_length=11)
+
     def __str__(self):
         return self.station_name
-    
+
 
 class Train(models.Model):
     '''Actual train'''
@@ -20,16 +20,20 @@ class Train(models.Model):
     model_type = models.CharField(max_length=50)
     status = models.CharField(max_length=50)
     info = models.URLField('additional information', max_length=200)
+
     def __str__(self):
         return str(self.number)
 
 
 class DepartureTime(models.Model):
     '''Departure time of a train at a specific station'''
+    #date = timezone.datetime(timezone.now().year, timezone.now().month, timezone.now().day, 17, 00)
     departure_time = models.DateTimeField('departure time')
     station = models.ForeignKey(Station)
     train = models.ForeignKey(Train)
+
     def __str__(self):
-        return self.str(self.departure_time)
-    def isLeavingBeforeXMinutes(self, nb_minutes):
-        return timezone.now() + timedelta(minutes=nb_minutes) > self.departure_time
+        return str(self.departure_time)
+
+    def isLeavingInNextXMinutes(self, nb_minutes):
+        return timezone.now() + timezone.timedelta(minutes=nb_minutes) >= self.departure_time
