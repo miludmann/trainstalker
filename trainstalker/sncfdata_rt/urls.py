8f8eeea970273e3ab2ab458c9f02971c911da506
Michael Ludmann <miludmann@gmail.com>
2015-01-28 18:08:44 +0100

    Add more views

--- trainstalker/sncfdata_rt/urls.py
+++ trainstalker/sncfdata_rt/urls.py
@@ -3,5 +3,13 @@ from django.conf.urls import patterns, url
 from sncfdata_rt import views
 
 urlpatterns = patterns('',
-                       url(r'^$', views.index, name='index'),
+        # ex: /sncfdata_rt/
+        url(r'^$', views.index, name='index'),
+        # ex: /sncfdata_rt/OCE87757674
+        url(r'^(?P<station_code>[A-Z0-9]+)/$', views.station, name='station'),
+        # ex: /sncfdata_rt/OCE87757674/train
+        url(r'^(?P<number>[A-Z0-9]+)/train/$', views.train, name='train'),
+        # ex: /sncfdata_rt/OCE87757674/departure
+        url(r'^(?P<departure_time>[A-Z0-9]+)/$/departure',
+            views.departure, name='departure'),
                        )
--- trainstalker/sncfdata_rt/views.py
+++ trainstalker/sncfdata_rt/views.py
@@ -7,3 +7,15 @@ def index(request):
     return HttpResponse("You are at the real-time train data index.")
 
 
+def train(request, number):
+    response = "You are looking at train from station %s."
+    return HttpResponse(response % number)
+
+def station(request, station_code):
+    response = "You are looking at train station %s."
+    return HttpResponse(response % station_code)
+
+
+def departure(request, departure_time):
+    response = "You are looking at departure time from station %s."
+    return HttpResponse(response % departure_time)
